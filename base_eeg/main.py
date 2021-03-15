d40d0bb068a8598d6a456a3d087ef331be82b61d
Jonhas Colina <jcoli015@fiu.edu>
2021-03-15 22:08:10 +0000

    feature to read raw egi successful through commandline, however, writing bids gives error due to signal1.bin file within the mff file

---
+++ base_eeg/main.py
@@ -0,0 +1,33 @@
+import preprocessing
+import mne
+from mne_bids import BIDSPath, write_raw_bids
+from matplotlib import pyplot as plt 
+import argparse, sys, os 
+
+# from mne.datasets import sample
+# from mne_bids import BIDSPath
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preprocessing Script for EEG Data")
+    parser.add_argument(
+        '--fname', 
+        help="Path to raw EGI file",
+        type=str, 
+    )
+
+    args = parser.parse_args()
+    if not args.fname:
+        parser.print_usage()
+        sys.exit(1)
+
+    raw = preprocessing.read_raw(args.fname)
+    
+    bids_path = BIDSPath(
+        subject="01",
+        session="01",
+        task="testing",
+        run="01", 
+        root="/data/BIDS"
+    )
+    
+    bids = preprocessing.write_bids(raw, bids_path)
---
+++ base_eeg/preprocessing.py
@@ -0,0 +1,25 @@
+import mne
+import os
+from mne_bids import write_raw_bids
+"""
+    Wrapper function to read data from EEG devices as a raw object dependent on
+    raw binary's type. Can be extended to include several eeg data types, such as 
+    BrainVision and EGI. 
+"""
+
+
+def read_raw(fname: str) -> mne.io.Raw:
+    _, ext = os.path.splitext(fname)
+    if(ext == ".mff"):
+        return mne.io.read_raw_egi(fname)
+    else:
+        return
+
+
+
+def write_bids(raw, bids_path):
+    return write_raw_bids(
+        raw, 
+        bids_path,
+        verbose=True 
+    )
