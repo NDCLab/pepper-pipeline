11102cd177658ef6ba6c04b5a38e8e876165388f
Jonhas Colina <jcoli015@fiu.edu>
2021-04-01 23:16:58 +0000

    Applied necessary changes to fix pybv issue. Cannot include BidsPath for it is too heavy for github. Users can generate their own BidsPath running   python3 main.py --fname fileName.MFF

--- base_eeg/main.py
+++ base_eeg/main.py
@@ -9,6 +9,7 @@ Main scripting file for the preprocessing system
 import preprocessing
 from mne_bids import BIDSPath
 import argparse
+import mne
 import sys
 
 if __name__ == "__main__":
@@ -29,6 +30,20 @@ if __name__ == "__main__":
         sys.exit(1)
 
     raw = preprocessing.read_raw(args.fname)
+
+    # Use the aggregate stimulation channel 'STI 014' to create
+    # an events structure
+    events = mne.find_events(raw, stim_channel="STI 014", shortest_event=1)
+    event_annotations = mne.annotations_from_events(
+        events=events,
+        sfreq=raw.info['sfreq'],
+        orig_time=raw.info['meas_date']
+    )
+
+    # add annotations to existing annotations (the bad acquisition skips)
+    # and keep only the EEG channels, removing the stimulation channels
+    raw.set_annotations(raw.annotations + event_annotations)
+    raw.pick_types(eeg=True)
 
     # Building the BidsPath which allows dynamic updating of entities in place
     bids_path = BIDSPath(
