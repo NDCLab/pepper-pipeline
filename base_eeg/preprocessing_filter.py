cbfaefb5f9db60325364be701d2bf006d4922e6c
Yanbin <yn2352@tc.columbia.edu>
2021-04-18 23:30:39 +0800

    filter feature

--- base_eeg/preprocessing_filter.py
+++ base_eeg/preprocessing_filter.py
@@ -1,3 +1,5 @@
+import matplotlib
+import pathlib
 import mne
 import mne_bids
 
@@ -5,7 +7,23 @@ def baseeeg_preprocessing_filter(raw: mne.io.Raw, l_freq=0.3, h_freq=40) -> mne.
     try:
         raw.load_data()
         return raw.copy().filter(l_freq=l_freq, h_freq=h_freq)
+        
     except TypeError:
         print('Type Error')
     except Exception:
         print('Unknown Error')
+
+# Read BIDS data
+bids_root = pathlib.Path('raw_data/eegmatchingpennies')
+bids_path = mne_bids.BIDSPath(subject='05',
+                              task='matchingpennies',
+                              datatype='eeg',
+                              root=bids_root)
+
+raw = mne_bids.read_raw_bids(bids_path)
+raw.plot()
+
+# filter
+raw_filtered = baseeeg_preprocessing_filter(raw)
+
+raw_filtered.plot()
