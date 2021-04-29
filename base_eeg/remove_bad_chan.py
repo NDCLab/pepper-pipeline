663f836f1c9d6e650a85475f452311993b6e78b6
unknown <saidmf@rpi.edu>
2021-04-29 12:06:47 -0500

    Standardized function documentation

--- base_eeg/remove_bad_chan.py
+++ base_eeg/remove_bad_chan.py
@@ -1,18 +1,16 @@
-import matplotlib
-import pathlib
-import mne
-import mne_bids
-import autoreject as ar 
+import matplotlib.pyplot as plt 
+from pathlib import Path
 
-import logging
+from mne.channels import make_standard_montage
+from mne import events_from_annotations, Epochs
+from mne_bids import BIDSPath, read_raw_bids
+
 import sys
-import os 
-import json 
 
 def main(): 
     # import raw data 
-    bids_root = pathlib.Path('BIDS')
-    bids_path = mne_bids.BIDSPath(
+    bids_root = Path('BIDS')
+    bids_path = BIDSPath(
         subject='NDARAB793GL3', 
         session='01', 
         task='ContrastChangeBlock1',
@@ -20,35 +18,43 @@ def main():
         datatype='eeg', 
         root=bids_root
     )
-    raw = mne_bids.read_raw_bids(bids_path)
-    montage = mne.channels.make_standard_montage("standard_1020")
+    raw = read_raw_bids(bids_path)
+
+    # set standard montage 
+    montage = make_standard_montage("standard_1020")
     raw.set_montage(montage)
 
+    # create epochs
+    events, event_id = events_from_annotations(raw)
+    epochs = Epochs(raw, events, event_id=event_id, preload=True)
+
     # Mark and list bad channels 
-    # remove_bad_channels(epochs, raw)
+    remove_bad_channels(epochs, raw)
 
-def remove_bad_channels(epochs: mne.Epochs, raw: mne.io.Raw) -> None:
-    """Automatically detect bad channels and mark to ignore during analysis 
+def remove_bad_channels(epochs, raw):
+    """Detects and marks bad channels in "raw" data-set. Modifies in place
 
-    @ modifies: raw 
-    @ throws: 
-        - "Exception" exception if "raw" is not an instance of "mne.io.Raw"
-    @ postconditions: bad channels are annotated as such within "raw" dataset 
+    Parameters
+    ----------
+    epochs: mne.Epochs
+        Data containing segmented raw dataset into epochs
+    raw: mne.io.Raw
+        Data containing raw EEG data that has been initliazed pre function call
+
+    Throws
+    ----------
+    "Exception" exception if "raw" is not an instance of "mne.io.Raw"
+    "Exception" exception if "epochs" is not an instance of "mne.io.Raw"
+
+    
     """
     try:
         raw.load_data()
-    except Exception:
+    except TypeError:
         print("\"raw\" data must be an instance of mne.io.Raw. exiting")
         sys.exit(1)
 
-    # Init logging file to record warnings and errors
-    logging.basicConfig(filename='output.log', filemode='a', level=logging.NOTSET)
-
-    reject = ar.AutoReject()
-    reject.fit(epochs)
-    reject_log = reject.get_reject_log(epochs)
-
-    names = reject_log.ch_names
+    names = raw.ch_names
     print("rejection log:", names)
 
     for label in names:
