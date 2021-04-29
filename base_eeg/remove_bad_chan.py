3474b07e616ccf76772a072832d2d3950c8f08b1
unknown <saidmf@rpi.edu>
2021-04-29 14:24:45 -0500

    Imported FASTER gist file wmvanvliet, replaced deprec functions, ran on bids data

--- base_eeg/remove_bad_chan.py
+++ base_eeg/remove_bad_chan.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
-from pathlib import Path
+from pathlib import Path 
 
 from mne.channels import make_standard_montage
 from mne import events_from_annotations, Epochs
 from mne_bids import BIDSPath, read_raw_bids
 
-import sys
+from faster import faster_bad_channels
 
 def main(): 
     # import raw data 
@@ -19,46 +19,17 @@ def main():
         root=bids_root
     )
     raw = read_raw_bids(bids_path)
-
+    
     # set standard montage 
-    montage = make_standard_montage("standard_1020")
-    raw.set_montage(montage)
+    # montage = make_standard_montage("standard_1020")
+    # raw.set_montage(montage)
 
-    # create epochs
     events, event_id = events_from_annotations(raw)
     epochs = Epochs(raw, events, event_id=event_id, preload=True)
 
     # Mark and list bad channels 
-    remove_bad_channels(epochs, raw)
-
-def remove_bad_channels(epochs, raw):
-    """Detects and marks bad channels in "raw" data-set. Modifies in place
-
-    Parameters
-    ----------
-    epochs: mne.Epochs
-        Data containing segmented raw dataset into epochs
-    raw: mne.io.Raw
-        Data containing raw EEG data that has been initliazed pre function call
-
-    Throws
-    ----------
-    "Exception" exception if "raw" is not an instance of "mne.io.Raw"
-    "Exception" exception if "epochs" is not an instance of "mne.io.Raw"
-
-    
-    """
-    try:
-        raw.load_data()
-    except TypeError:
-        print("\"raw\" data must be an instance of mne.io.Raw. exiting")
-        sys.exit(1)
-
-    names = raw.ch_names
-    print("rejection log:", names)
-
-    for label in names:
-        raw.drop_channels(label)
+    bad_channels = faster_bad_channels(epochs)
+    print(bad_channels)
 
 if __name__ == "__main__":
     main()
