eeaa82b1d34b5ec6d47734b52ae2f5f7a908a848
unknown <saidmf@rpi.edu>
2021-04-29 14:33:34 -0500

    removed flake8 errors from bad_chans

--- base_eeg/remove_bad_chan.py
+++ base_eeg/remove_bad_chan.py
@@ -1,35 +1,31 @@
-import matplotlib.pyplot as plt 
-from pathlib import Path 
+from pathlib import Path
 
-from mne.channels import make_standard_montage
 from mne import events_from_annotations, Epochs
 from mne_bids import BIDSPath, read_raw_bids
 
 from faster import faster_bad_channels
 
-def main(): 
-    # import raw data 
+
+def main():
+    # import raw data
     bids_root = Path('BIDS')
     bids_path = BIDSPath(
-        subject='NDARAB793GL3', 
-        session='01', 
+        subject='NDARAB793GL3',
+        session='01',
         task='ContrastChangeBlock1',
-        run='01', 
-        datatype='eeg', 
+        run='01',
+        datatype='eeg',
         root=bids_root
     )
     raw = read_raw_bids(bids_path)
-    
-    # set standard montage 
-    # montage = make_standard_montage("standard_1020")
-    # raw.set_montage(montage)
 
     events, event_id = events_from_annotations(raw)
     epochs = Epochs(raw, events, event_id=event_id, preload=True)
 
-    # Mark and list bad channels 
+    # mark and list bad channels
     bad_channels = faster_bad_channels(epochs)
     print(bad_channels)
+
 
 if __name__ == "__main__":
     main()
