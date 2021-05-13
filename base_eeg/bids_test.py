46c9e9c141c4deb0f0de88b27e2fae7dead1efd8
unknown <saidmf@rpi.edu>
2021-05-13 11:04:46 -0500

    Incorperated edits to subTest & strip root name

--- base_eeg/bids_test.py
+++ base_eeg/bids_test.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from os import walk
+from os import walk, sep
 from os.path import join
 
 from bids_validator import BIDSValidator
@@ -18,6 +18,9 @@ class BidsTest(unittest.TestCase):
     self.files: list
         list that contains all data files from BIDS directory
 
+    self.root: str
+        BIDS directory root
+
     Public Methods
     ----------
     test_if_valid()
@@ -28,19 +31,22 @@ class BidsTest(unittest.TestCase):
         # init validator
         self.validator = BIDSValidator()
 
-        root = Path('BIDS')
+        self.root = Path('BIDS')
         self.files = []
         # "walk" through BIDS directory and append all found files
-        for (dirpath, dirnames, filenames) in walk(root):
+        for (dirpath, dirnames, filenames) in walk(self.root):
             path_filenames = [join(dirpath, name) for name in filenames]
             self.files += path_filenames
 
     def test_if_valid(self):
         # for each file found, inspect if BIDS
         for file in self.files:
-            print("checking", file)
-            self.assertTrue(self.validator.is_bids(file[4:]),
-                            msg="BIDS validate failed on file" + str(file[4:]))
+            with self.subTest(file=file):
+                print("checking", file)
+                relative_path = join(sep, Path(file).relative_to(self.root))
+                self.assertTrue(self.validator.is_bids(relative_path),
+                                msg="BIDS validate failed on file "
+                                + relative_path)
 
 
 if __name__ == '__main__':
