e08bc3ad6011630e413d38f9ae29808af8e3b75c
unknown <saidmf@rpi.edu>
2021-05-18 14:11:12 -0500

    removed raw from final_reject for pipeline integration

--- base_eeg/finalRej.py
+++ base_eeg/finalRej.py
@@ -1,49 +1,48 @@
-import mne
 import autoreject as ar
 import pandas as pd
 import collections
 
 
-def final_reject_epoch(user_params_dict, epochs, raw):
+def final_reject_epoch(epochs):
     """Final and automatic rejection of bad epochs
     Parameters
     ----------
-    user_params_dict : python dictionary
-        user defined variables like tmin and tmax,
     epochs: mne.Epochs object
-        instance of the mne.Epochs,
-    raw: mne.io.Raw object
-        data as mne Raw object
+            instance of the mne.Epochs,
 
-    Returns 
+    Returns
     ----------
-    output_dict_finalRej: dictionary
-        dictionary with epochs droped per channel and channels interpolated
+    epochs_clean:   mne.Epochs object
+                    instance of "cleaned" epochs using autoreject
+
+    output_dict_finalRej:   dictionary
+                            dictionary with epochs droped per channel and
+                            channels interpolated
     """
-    
-    #creates the output dictionary to store the function output
+
+    # creates the output dictionary to store the function output
     output_dict_finalRej = collections.defaultdict(dict)
-    output_dict_finalRej['interpolatedChannels']=[]
+    output_dict_finalRej['interpolatedChannels'] = []
 
-    #fit epoch data using autoreject
+    # fit and clean epoch data using autoreject
     autoRej = ar.AutoReject()
-    autoRej.fit(epochs)
+    epochs_clean = autoRej.fit_transform(epochs)
 
-    #Create a rejection log 
+    # Create a rejection log
     reject_log = autoRej.get_reject_log(epochs)
-    #Create a dataframe with the labels that indicate if a channel was interpolated
-    df = pd.DataFrame(data=reject_log.labels,columns=raw.info.ch_names)
-    for ch in raw.info.ch_names:
-        if df[df[ch]==2][ch].count()>0:
+
+    # get channel names
+    ch_names = epochs.info.ch_names
+
+    # Create dataframe with the labels of which channel was interpolated
+    df = pd.DataFrame(data=reject_log.labels, columns=ch_names)
+    for ch in ch_names:
+        if df[df[ch] == 2][ch].count() > 0:
             output_dict_finalRej['interpolatedChannels'].append(ch)
 
-    # Get rejection dictionary to reject automatically detected bad epochs
-    reject = ar.get_rejection_threshold(epochs)  
-    
-    for ch in raw.info.ch_names:
-        rej_epochs = mne.Epochs(raw, epochs.events, event_id=epochs.event_id, tmin=user_params_dict['tmin'], tmax=user_params_dict['tmax'], #tmin=-0.2 tmax=0.5
-                            picks=ch, preload=True, reject=reject, reject_by_annotation=True)
-        
+    for ch in ch_names:
         # store amount of epochs dropped for each channel
-        output_dict_finalRej['epochsDropped'][ch]=str(rej_epochs.drop_log.count((ch,)))
-    return output_dict_finalRej
+        output_dict_finalRej['epochsDropped'][ch] = str(
+            epochs_clean.drop_log.count((ch,)))
+
+    return epochs_clean, output_dict_finalRej
--- base_eeg/test.py
+++ base_eeg/test.py
@@ -1,32 +1,30 @@
 import mne
 import pathlib
-import json
-import autoreject as ar
-from find_montage import get_montage
 from mne_bids import BIDSPath, read_raw_bids
 import finalRej as fr
-    
+
+
 def main():
-    # import raw data 
+    # import raw data
     bids_root = pathlib.Path('BIDS')
     bids_path = BIDSPath(subject='NDARAB793GL3',
-        session='01',
-        task='ContrastChangeBlock1',
-        run='01',
-        datatype='eeg', root=bids_root)
+                         session='01',
+                         task='ContrastChangeBlock1',
+                         run='01',
+                         datatype='eeg', root=bids_root)
     raw = read_raw_bids(bids_path)
-    # Generate events to segment data into epochs 
+
+    # Generate events to segment data into epochs
     events, event_id = mne.events_from_annotations(raw)
     epochs = mne.Epochs(raw, events, event_id=event_id, preload=True)
+
     # Set montage
     easy_montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
     epochs.set_montage(easy_montage)
 
-    #Sample user parameter 
-    user_params={'tmin':-0.2,'tmax':0.5}
-    output_dict_finalRej=fr.final_reject_epoch(user_params, epochs, raw)
-    print('this is the the output dictionary ---> ',output_dict_finalRej)
+    # Sample user parameter
+    output_dict_finalRej = fr.final_reject_epoch(epochs)
+    print('this is the the output dictionary ---> ', output_dict_finalRej)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
