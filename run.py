f15f0497a745333edc97f7ee2791871e2cbd43fd
unknown <saidmf@rpi.edu>
2021-05-19 14:46:51 -0500

    Explicitly import preprocess steps in run module to run

--- run.py
+++ run.py
@@ -2,11 +2,15 @@ from scripts.data import load_params
 from scripts.data import load_raw
 from scripts.data import write_annotate
 
-import scripts.preprocess.filter
-import scripts.preprocess.segment_data
-import scripts.preprocess.final_reject_epoch
-import scripts.preprocess.interpolate_data
+from scripts.preprocess.filter import filter 
+from scripts.preprocess.segment_data import segment_data
+from scripts.preprocess.final_reject_epoch import final_reject_epoch
+from scripts.preprocess.interpolate_data import interpolate_data
+
 from collections import ChainMap
+import sys
+
+current_module = sys.modules[__name__]
 
 # loop over the functions
 user_params = load_params.user_params
@@ -14,7 +18,7 @@ func_outputs = [None] * len(user_params)
 eeg_obj = load_raw.raw
 
 for idx, (func, params) in enumerate(user_params.items()):
-    eeg_obj, func_outputs[idx] = getattr(scripts.preprocess.func, func)(eeg_obj, **params)
+    eeg_obj, func_outputs[idx] = getattr(current_module, func)(eeg_obj, **params)
 
 # and collect their outputs
 output = dict(ChainMap(*func_outputs))
