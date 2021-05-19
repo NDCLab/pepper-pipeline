0606889823b13b90798c1d31f0f5112fb8727763
unknown <saidmf@rpi.edu>
2021-05-19 14:35:22 -0500

    Added user_params JSON file

--- run.py
+++ run.py
@@ -1,17 +1,21 @@
-from load_params import user_params
-from load_data import raw 
-import dictCollat as out
-import pipeline
+from scripts.data import load_params
+from scripts.data import load_raw
+from scripts.data import write_annotate
 
+import scripts.preprocess.filter
+import scripts.preprocess.segment_data
+import scripts.preprocess.final_reject_epoch
+import scripts.preprocess.interpolate_data
 from collections import ChainMap
 
 # loop over the functions
+user_params = load_params.user_params
 func_outputs = [None] * len(user_params)
-eeg_obj = raw
+eeg_obj = load_raw.raw
 
 for idx, (func, params) in enumerate(user_params.items()):
-    eeg_obj, func_outputs[idx] = getattr(pipeline, func)(eeg_obj, **params)
+    eeg_obj, func_outputs[idx] = getattr(scripts.preprocess.func, func)(eeg_obj, **params)
 
 # and collect their outputs
 output = dict(ChainMap(*func_outputs))
-out.read_dict_to_json(output)
+write_annotate.read_dict_to_json(output)
