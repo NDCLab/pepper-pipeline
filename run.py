5793586e0828399ed34cb29f0ed42fe4fc94efe3
unknown <saidmf@rpi.edu>
2021-05-21 10:53:08 -0500

    added comments for documentation

--- run.py
+++ run.py
@@ -5,14 +5,15 @@ from scripts.data import write_annotate
 import scripts.preprocess.preprocess as preproc
 from collections import ChainMap
 
-# loop over the functions
+# get user_params from user_params.json
 user_params = load_params.user_params
+
+# for each pipeline step in user_params, execute with specified parameters
 func_outputs = [None] * len(user_params)
 eeg_obj = load_raw.raw
-
 for idx, (func, params) in enumerate(user_params.items()):
     eeg_obj, func_outputs[idx] = getattr(preproc, func)(eeg_obj, **params)
 
-# and collect their outputs
+# collect outputs of each step and annotate changes
 output = dict(ChainMap(*func_outputs))
 write_annotate.read_dict_to_json(output)
