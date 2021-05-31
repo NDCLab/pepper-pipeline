98ced7698efbee27b00cc51e919591c834600893
unknown <saidmf@rpi.edu>
2021-05-31 12:07:19 -0500

    Updated run with load_data functions

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
-from scripts.data import load_params
-from scripts.data import load_raw
+from scripts.data import load_data
 from scripts.data import write_annotate
 
 import scripts.preprocess.preprocess as preproc
 from collections import ChainMap
 
 # get user_params from user_params.json
-user_params = load_params.user_params
+user_params = load_data.load_param("user_params.json")
 
 # for each pipeline step in user_params, execute with specified parameters
 func_outputs = [None] * len(user_params)
-eeg_obj = load_raw.raw
+eeg_obj = load_data.load_raw(
+    "CMI/rawdata/",
+    "NDARAB793GL3",
+    "01",
+    "ContrastChangeBlock1",
+    "01",
+    "eeg"
+)
 for idx, (func, params) in enumerate(user_params.items()):
     eeg_obj, func_outputs[idx] = getattr(preproc, func)(eeg_obj, **params)
 
