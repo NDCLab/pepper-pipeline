17d317953b6909025f5ea7fb4d10d78e434dbf1d
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-06-24 16:24:49 -0400

    integrated reref.py into preprocess and deleted the file

--- scripts/preprocess/preprocess.py
+++ scripts/preprocess/preprocess.py
@@ -9,6 +9,54 @@ from mne.preprocessing.bads import _find_outliers
 from scipy.stats import zscore
 
 
+def reref_raw(raw, ref_channels=None):
+    """Re-reference the data to the average of all electrodes
+    Parameters
+    ----------
+    raw:    mne.io.Raw
+            raw object of EEG data
+    ref_channels: list
+            list of reference channels
+
+    Throws
+    ----------
+    TypeError:
+                returns if raw is improper type
+    Exception:
+                returns if unexpected error is encountered
+    Returns
+    ----------
+    raw_filtered:   mne.io.Raw
+                    instance of rereferenced data
+    output_dict_reference:  dictionary
+                            dictionary with relevant information on re-ref
+    """
+    try:
+        raw.load_data()
+
+        # add back reference channel (all zero)
+        if ref_channels is None:
+            raw_new_ref = raw
+        else:
+            raw_new_ref = mne.add_reference_channels(raw,
+                                                     ref_channels=ref_channels)
+
+        ref_type = "average"
+        raw_new_ref = raw_new_ref.set_eeg_reference(ref_channels=ref_type)
+
+        ref_details = {
+            "Reference Type": ref_type
+        }
+        # return average reference
+        return raw_new_ref, {"Reference": ref_details}
+
+    except TypeError:
+        print('Type Error')
+
+    except Exception:
+        print('Unknown Error')
+
+
 def filter_data(raw, l_freq=0.3, h_freq=40):
     """Final and automatic rejection of bad epochs
     Parameters
