dc1a83de44f1a9fd5a04f9694a4b99c2aafbd2fe
unknown <saidmf@rpi.edu>
2021-09-13 14:24:14 -0400

    Added exception messages for missing data, pipeline exit, and subject skip

--- scripts/constants.py
+++ scripts/constants.py
@@ -4,28 +4,29 @@ INTERM = "_intermediate"
 FINAL = "_preprocessed"
 
 # Load data error messages
-MISSING_PATH_MSG = "Missing Path. Please check if file or directory exists."
-INVALID_DATA_MSG = "Please use mne.io.Raw or mne.Epochs datatype \
-    as raw."
-INVALID_UPARAM_MSG = "Please use a valid user_param file. A valid template can be \
-    created using data:write.write_template_params()"
-INVALID_SUBJ_PARAM_MSG = "Please populate the subject parameter w/correct values. \
-    A valid template can be created using data:write.write_template_params()"
-INVALID_TASK_PARAM_MSG = "Please populate the task parameter w/correct values. \
-    A valid template can be created using data:write.write_template_params()"
-INVALID_E_SUBJ_MSG = "Please populate the exception subject parameter w/correct values. \
-    A valid template can be created using data:write.write_template_params()"
-INVALID_E_TASK_MSG = "Please populate the exception task parameter w/correct values. \
-    A valid template can be created using data:write.write_template_params()"
-INVALID_E_RUN_MSG = "Please populate the exception run parameter w/correct values. \
-    A valid template can be created using data:write.write_template_params()"
+MISSING_PATH_MSG = "Missing path. Please check if file or directory exists."
+MISSING_DATA_MSG = "Missing data at path. Please check if data at path exists\
+ and is a valid BIDS directory."
+INVALID_DATA_MSG = "Please use mne.io.Raw or mne.Epochs datatype\
+ as raw."
+INVALID_UPARAM_MSG = "Please use a valid user_param file. A valid template can be\
+ created using data:write.write_template_params()"
+INVALID_SUBJ_PARAM_MSG = "Please populate the subject parameter w/correct values.\
+ A valid template can be created using data:write.write_template_params()"
+INVALID_TASK_PARAM_MSG = "Please populate the task parameter w/correct values.\
+ A valid template can be created using data:write.write_template_params()"
+INVALID_E_SUBJ_MSG = "Please populate the exception subject parameter w/correct values.\
+ A valid template can be created using data:write.write_template_params()"
+INVALID_E_TASK_MSG = "Please populate the exception task parameter w/correct values.\
+ A valid template can be created using data:write.write_template_params()"
+INVALID_E_RUN_MSG = "Please populate the exception run parameter w/correct values.\
+ A valid template can be created using data:write.write_template_params()"
 
 # Preprocess feature error messages/exceptions
 INVALID_FILTER_FREQ_MSG = "'Please use sufficiently seperated floats for \
-            l_freq & h_freq."
+ l_freq & h_freq."
 INVALID_FR_DATA_MSG = "The least populated class in y has only 1 member, which is too\
-            few. The minimum number of groups for any class cannot be\
-            less than 2."
+ few. The minimum number of groups for any class cannot be less than 2."
 INVALID_MONTAGE_MSG = "Invalid value for the 'montage' parameter. Allowed values are 'EGI_256', \
         'GSN-HydroCel-128', 'GSN-HydroCel-129', 'GSN-HydroCel-256',\
         'GSN-HydroCel-257', 'GSN-HydroCel-32', 'GSN-HydroCel-64_1.0',\
@@ -35,6 +36,11 @@ INVALID_MONTAGE_MSG = "Invalid value for the 'montage' parameter. Allowed values
         'standard_alphabetic', 'standard_postfixed', 'standard_prefixed',\
         'standard_primed', 'artinis-octamon', and 'artinis-brite23'."
 
+# write error messages
+SKIP_REWRITE_MSG = "File already exists. Skipping write according to 'rewrite'\
+ parameter."
+
 # main driver code error messages
-CAUGHT_EXCEPTION_SKIP = ": Exception caught on function. Skipping to next \
-     subject."
+CAUGHT_EXCEPTION_SKIP = ": Exception caught on function. Skipping to next\
+ subject."
+EXIT_MESSAGE = "Pipeline exited according to 'exit_on_error' parameter."
