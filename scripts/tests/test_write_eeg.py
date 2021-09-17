e31112468c0d00450ff44f2c437b95dfd183d1fe
unknown <saidmf@rpi.edu>
2021-09-17 15:26:05 -0400

    Created test for write_eeg_data with default case

--- scripts/tests/test_write_eeg.py
+++ scripts/tests/test_write_eeg.py
@@ -6,7 +6,12 @@ from pathlib import Path
 import os
 
 import mne_bids
-import pandas as pd
+
+
+@pytest.fixture
+def root():
+    root = Path("CMI/rawdata")
+    return root
 
 
 @pytest.fixture
@@ -26,9 +31,10 @@ def select_task():
 
 
 @pytest.fixture
-def temp_root(tmp_path):
+def write_root(tmp_path):
     temp_path = str(tmp_path) + "CMI"
     os.mkdir(temp_path)
+    return temp_path
 
 
 @pytest.fixture
@@ -36,18 +42,21 @@ def tmp_func():
     return "TEMP"
 
 
-def test_write(default_param, select_subj, select_task, temp_root, tmp_func):
+def test_write(default_param, select_subj, select_task, write_root, tmp_func):
     data_params = default_param["load_data"]
 
-    data_params["root"] = temp_root
     data_params["subjects"] = select_subj
     data_params["tasks"] = select_task
 
     ch_type = data_params["channel-type"]
-    rewrite = data_params["rewrite"]
+    rewrite = data_params["overwrite"]
 
     data = load.load_files(default_param["load_data"])
     for file in data:
         eeg_obj = mne_bids.read_raw_bids(file)
-        write.write_eeg_data(eeg_obj, tmp_func, file, ch_type, 0, temp_root,
+        write.write_eeg_data(eeg_obj, tmp_func, file, ch_type, 0, write_root,
                              rewrite)
+        for _, dirnames, filenames in os.walk(write_root):
+            # if at bottom-most directory, assert one file has been written
+            if not dirnames:
+                assert len(filenames) == 1
