f583fd553114474f52dd1f005fdff98bd436b4f0
unknown <saidmf@rpi.edu>
2021-09-20 11:48:22 -0400

    Condensed data selection into fixture

--- scripts/tests/test_write_eeg.py
+++ scripts/tests/test_write_eeg.py
@@ -15,19 +15,32 @@ def root():
 
 
 @pytest.fixture
-def default_param(root):
+def default_params(root):
     default_param = write.write_template_params(root)
     return default_param
 
 
 @pytest.fixture
-def select_subj():
+def overwrite_params(default_params):
+    default_params["load_data"]["rewrite"] = False
+    return default_params
+
+
+@pytest.fixture
+def sel_subjects():
     return ["NDARAB793GL3"]
 
 
 @pytest.fixture
-def select_task():
+def sel_tasks():
     return ["ContrastChangeBlock1"]
+
+
+@pytest.fixture
+def select_data_params(default_params, sel_subjects, sel_tasks):
+    default_params["load_data"]["subjects"] = sel_subjects
+    default_params["load_data"]["tasks"] = sel_tasks
+    return default_params
 
 
 @pytest.fixture
@@ -42,16 +55,30 @@ def tmp_func():
     return "TEMP"
 
 
-def test_write(default_param, select_subj, select_task, write_root, tmp_func):
-    data_params = default_param["load_data"]
-
-    data_params["subjects"] = select_subj
-    data_params["tasks"] = select_task
+def test_write(select_data_params, write_root, tmp_func):
+    data_params = select_data_params["load_data"]
 
     ch_type = data_params["channel-type"]
     rewrite = data_params["overwrite"]
 
-    data = load.load_files(default_param["load_data"])
+    data = load.load_files(data_params)
+    for file in data:
+        eeg_obj = mne_bids.read_raw_bids(file)
+        write.write_eeg_data(eeg_obj, tmp_func, file, ch_type, 0, write_root,
+                             rewrite)
+        for _, dirnames, filenames in os.walk(write_root):
+            # if at bottom-most directory, assert one file has been written
+            if not dirnames:
+                assert len(filenames) == 1
+
+
+def test_overwrite(select_data_params, write_root, tmp_func):
+    data_params = select_data_params["load_data"]
+
+    ch_type = data_params["channel-type"]
+    rewrite = data_params["overwrite"]
+
+    data = load.load_files(data_params)
     for file in data:
         eeg_obj = mne_bids.read_raw_bids(file)
         write.write_eeg_data(eeg_obj, tmp_func, file, ch_type, 0, write_root,
