5b357aa608c1c6a9be87af4c513c6d4095a85c96
unknown <saidmf@rpi.edu>
2021-09-20 14:11:49 -0400

    Added tests for non-path and non-overwrite

--- scripts/tests/test_write_eeg.py
+++ scripts/tests/test_write_eeg.py
@@ -21,14 +21,6 @@ def default_params(root, tmp_path):
 
 
 @pytest.fixture
-def overwrite_params(default_params):
-    default_params["load_data"]["subjects"] = sel_subjects
-    default_params["load_data"]["tasks"] = sel_tasks
-    default_params["load_data"]["rewrite"] = False
-    return default_params
-
-
-@pytest.fixture
 def sel_subjects():
     return ["NDARAB793GL3"]
 
@@ -46,10 +38,15 @@ def select_data_params(default_params, sel_subjects, sel_tasks):
 
 
 @pytest.fixture
-def write_root(tmp_path):
-    temp_path = str(tmp_path) + "CMI"
-    os.mkdir(temp_path)
-    return temp_path
+def overwrite_params(select_data_params):
+    select_data_params["load_data"]["overwrite"] = False
+    return select_data_params
+
+
+@pytest.fixture
+def non_path_params(select_data_params, tmp_path):
+    select_data_params["output_data"]["root"] = tmp_path / "CMI"
+    return select_data_params
 
 
 @pytest.fixture
@@ -57,11 +54,13 @@ def tmp_func():
     return "TEMP"
 
 
-def test_write(select_data_params, write_root, tmp_func):
+def test_write(select_data_params, tmp_func):
     data_params = select_data_params["load_data"]
+    output_params = select_data_params["output_data"]
 
     ch_type = data_params["channel-type"]
     rewrite = data_params["overwrite"]
+    write_root = output_params["root"]
 
     data = load.load_files(data_params)
     for file in data:
@@ -74,11 +73,13 @@ def test_write(select_data_params, write_root, tmp_func):
                 assert len(filenames) == 1
 
 
-def test_overwrite(select_data_params, write_root, tmp_func):
+def test_overwrite(select_data_params, tmp_func):
     data_params = select_data_params["load_data"]
+    output_params = select_data_params["output_data"]
 
     ch_type = data_params["channel-type"]
     rewrite = data_params["overwrite"]
+    write_root = output_params["root"]
 
     data = load.load_files(data_params)
     for file in data:
@@ -86,10 +87,52 @@ def test_overwrite(select_data_params, write_root, tmp_func):
         # first write
         first_write = write.write_eeg_data(eeg_obj, tmp_func, file,
                                            ch_type, 0, write_root, rewrite)
-
         # overwrite
         overwrite = write.write_eeg_data(eeg_obj, tmp_func, file,
                                          ch_type, 0, write_root, rewrite)
 
         # assert first write produced string and second produced string again
         assert isinstance(first_write, str) and isinstance(overwrite, str)
+
+
+def test_non_overwrite(overwrite_params, tmp_func):
+    data_params = overwrite_params["load_data"]
+    output_params = overwrite_params["output_data"]
+
+    ch_type = data_params["channel-type"]
+    rewrite = data_params["overwrite"]
+    write_root = output_params["root"]
+
+    data = load.load_files(data_params)
+    for file in data:
+        eeg_obj = mne_bids.read_raw_bids(file)
+        # first write
+        first_write = write.write_eeg_data(eeg_obj, tmp_func, file,
+                                           ch_type, 0, write_root, rewrite)
+        # overwrite
+        overwrite = write.write_eeg_data(eeg_obj, tmp_func, file,
+                                         ch_type, 0, write_root, rewrite)
+
+        # assert first write produced string and second produced string again
+        assert isinstance(first_write, str) and (overwrite is None)
+
+
+def test_non_path(non_path_params, tmp_func):
+    data_params = non_path_params["load_data"]
+    output_params = non_path_params["output_data"]
+
+    ch_type = data_params["channel-type"]
+    rewrite = data_params["overwrite"]
+    write_root = output_params["root"]
+
+    data = load.load_files(data_params)
+    for file in data:
+        eeg_obj = mne_bids.read_raw_bids(file)
+        # first write
+        write.write_eeg_data(eeg_obj, tmp_func, file,
+                             ch_type, 0, write_root, rewrite)
+        # assert file exists even if path initially does not
+        for _, dirnames, filenames in os.walk(write_root):
+            # if at bottom-most directory, assert one file has been written
+            if not dirnames:
+                assert len(filenames) == 1
