6bf07cbb75e5106b2bcf19abc65cf474f2ede21d
Daniel Roberts <danmroberts@gmail.com>
2021-09-23 22:45:55 -0400

    Replace the list of valid montages with a ref to mne.channels.get_builtin_montages()

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,3 +1,5 @@
+import mne
+
 # Pipeline name constants
 PIPE_NAME = "PEPPER_pipeline"
 INTERM = "_intermediate"
@@ -27,14 +29,8 @@ INVALID_FILTER_FREQ_MSG = "'Please use sufficiently seperated floats for \
  l_freq & h_freq."
 INVALID_FR_DATA_MSG = "The least populated class in y has only 1 member, which is too\
  few. The minimum number of groups for any class cannot be less than 2."
-INVALID_MONTAGE_MSG = "Invalid value for the 'montage' parameter. Allowed values are 'EGI_256', \
-        'GSN-HydroCel-128', 'GSN-HydroCel-129', 'GSN-HydroCel-256',\
-        'GSN-HydroCel-257', 'GSN-HydroCel-32', 'GSN-HydroCel-64_1.0',\
-        'GSN-HydroCel-65_1.0', 'biosemi128', 'biosemi16', 'biosemi160',\
-        'biosemi256', 'biosemi32', 'biosemi64', 'easycap-M1', 'easycap-M10',\
-        'mgh60', 'mgh70', 'standard_1005', 'standard_1020',\
-        'standard_alphabetic', 'standard_postfixed', 'standard_prefixed',\
-        'standard_primed', 'artinis-octamon', and 'artinis-brite23'."
+INVALID_MONTAGE_MSG = f"Invalid value for the 'montage' parameter.\
+ Allowed values are: {', '.join(mne.channels.get_builtin_montages())}."
 
 MISSING_MONTAGE_MSG = "This operation requires a montage however the data does not have a montage applied. \
 Add a montage prior to this step via set_montage()."
