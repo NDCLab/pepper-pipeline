43153ac59eb54e8634437e1932d53dc26a1c5b39
unknown <saidmf@rpi.edu>
2021-10-01 08:11:00 -0400

    Added lines for constant vars for pep8

--- scripts/constants.py
+++ scripts/constants.py
@@ -9,20 +9,23 @@ FINAL = "_preprocessed"
 MISSING_PATH_MSG = "Missing path. Please check if file or directory exists."
 MISSING_DATA_MSG = "Missing data at path. Please check if data at path exists\
  and is a valid BIDS directory."
-INVALID_DATA_MSG = "Please use mne.io.Raw or mne.Epochs datatype\
+INVALID_DATA_MSG = "Invalid data type. Please use mne.io.Raw or mne.Epochs datatype\
  as raw."
-INVALID_UPARAM_MSG = "Please use a valid user_param file. A valid template can be\
- created using data:write.write_template_params()"
-INVALID_SUBJ_PARAM_MSG = "Please populate the subject parameter w/correct values.\
+INVALID_UPARAM_MSG = "Invalid pipeline parameter. Please use a valid user_param file. A \
+  valid template can be created using data:write.write_template_params()"
+INVALID_SUBJ_PARAM_MSG = "Invalid parameter. Please populate the subject parameter w/correct values.\
  A valid template can be created using data:write.write_template_params()"
-INVALID_TASK_PARAM_MSG = "Please populate the task parameter w/correct values.\
+INVALID_TASK_PARAM_MSG = "Invalid parameter. Please populate the task parameter w/correct values.\
  A valid template can be created using data:write.write_template_params()"
-INVALID_E_SUBJ_MSG = "Please populate the exception subject parameter w/correct values.\
- A valid template can be created using data:write.write_template_params()"
-INVALID_E_TASK_MSG = "Please populate the exception task parameter w/correct values.\
- A valid template can be created using data:write.write_template_params()"
-INVALID_E_RUN_MSG = "Please populate the exception run parameter w/correct values.\
- A valid template can be created using data:write.write_template_params()"
+INVALID_E_SUBJ_MSG = "Invalid parameter. Please populate the exception subject \
+ parameter w/correct values. A valid template can be created using \
+ data:write.write_template_params()"
+INVALID_E_TASK_MSG = "Invalid parameter. Please populate the exception task \
+ parameter w/correct values. A valid template can be created using \
+ data:write.write_template_params()"
+INVALID_E_RUN_MSG = "Invalid parameter. Please populate the exception run \
+ parameter w/correct values. A valid template can be created using \
+ data:write.write_template_params()"
 
 # Preprocess feature error messages/exceptions
 INVALID_FILTER_FREQ_MSG = "'Please use sufficiently seperated floats for \
@@ -32,8 +35,9 @@ INVALID_FR_DATA_MSG = "The least populated class in y has only 1 member, which i
 INVALID_MONTAGE_MSG = f"Invalid value for the 'montage' parameter.\
  Allowed values are: {', '.join(mne.channels.get_builtin_montages())}."
 
-MISSING_MONTAGE_MSG = "This operation requires a montage however the data does not have a montage applied. \
-Add a montage prior to this step via set_montage()."
+MISSING_MONTAGE_MSG = "This operation requires a montage however the data does \
+ not have a montage applied. Add a montage prior to this step via \
+ set_montage()."
 
 # write error messages
 SKIP_REWRITE_MSG = "File already exists. Skipping write according to 'rewrite'\
