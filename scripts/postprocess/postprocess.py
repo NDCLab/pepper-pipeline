aad4962688c81e464988fbd94e47ad1793e8dc7a
Yanbin <yn2352@tc.columbia.edu>
2021-11-05 18:23:31 +0800

    sme

--- scripts/postprocess/postprocess.py
+++ scripts/postprocess/postprocess.py
@@ -53,3 +53,70 @@ def get_trial_erp(filelist, start_t, end_t, cond1, cond2=None):
             datalist2.append(np.mean(epoch_con2_arr_int, axis=(1, 2)))
 
     return datalist1, datalist2
+
+
+def sd_sample_mean(values):
+    """get standard error of the sample mean
+    Parameters
+    ----------
+    values: np array
+            a list of sample values, shape - epochs * 1
+    Returns
+    ----------
+    sd_sm: float
+           standard error of the sample mean
+    """
+    return np.std(values, ddof=1) / np.sqrt(values.shape[0])
+
+
+def sme(filelist, start_t, end_t, cond, pick_elec):
+    """get trial level erp
+    Parameters
+    ----------
+    filelist: list
+              a list of file path in .fif format
+    start_t: float
+             start time point of interest (in second), e,g., 0, 0.1
+    end_t: float
+           end time point of interest (in second), e,g., 0.1, 0.2
+    cond: a list of string
+          names of the event of interest
+    pick_elec: a list of string
+               names of the electrode of interest
+    Returns
+    ----------
+    smelist: a list of list
+             a list of SME value for each participant and each condition
+    """
+    smelist = []
+
+    for dt in filelist:
+        raw = mne.read_epoch(dt)
+
+        # find the start and end time points of interest
+        start_point = bisect.bisect_left(raw.times, start_t)
+        end_point = bisect.bisect_left(raw.times, end_t)
+
+        # find the index of electrodes of interest
+        elec = [raw.ch_names.index(i) for i in pick_elec]
+
+        sem_arr = np.empty(len(cond), dtype=float)
+        # loop the condition list
+        for i, element in enumerate(cond):
+            # get epoch data within the condition of interest
+            # return a array of shape (n_epochs, n_channels, n_times)
+            epoch_con = raw[element]
+            epoch_con_dat = epoch_con.get_data()
+
+            # average across electrodes and time points of interest
+            epoch_con_dat_int = epoch_con_dat[:, elec, start_point:end_point]
+            if len(elec) == 1:
+                epoch_con_dat_int_mean = np.mean(epoch_con_dat_int, axis=1)
+            else:
+                epoch_con_dat_int_mean = np.mean(epoch_con_dat_int, axis=(1, 2))
+
+            # compute standard error of the sample mean
+            sem_arr[i] = sd_sample_mean(epoch_con_dat_int_mean)
+
+        smelist.append(sem_arr)
+    return smelist
--- scripts/postprocess/run_metrics.py
+++ scripts/postprocess/run_metrics.py
@@ -1,5 +1,5 @@
 import os
-import erp
+import postprocess
 
 data_path = "/Users/yanbinniu/Documents/Projects/tf-pca/OSF/tfpca-tutorial/ptb_data/"
 
@@ -9,7 +9,10 @@ for root, dirs, files in os.walk(data_path):
         if file.endswith('.fif'):
             fileList.append(os.path.join(root, file))
 
+# compute splithalf reliability
 # get trial level erp for all participants
-datalist1, datalist2 = erp.get_trial_erp(fileList, 0, 0.1, 'auditory', 'visual')
+datalist1, datalist2 = postprocess.get_trial_erp(fileList, 0, 0.1, 'auditory', 'visual')
+# call Dan's splithalf
 
-# cal Dan's splithalf
+# compute SME
+sme = postprocess.sme(fileList, 0, 0.1, ['auditory', 'visual'], ['E1', 'E2'])
