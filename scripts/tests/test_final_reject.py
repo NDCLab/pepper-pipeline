72a9325ef4075a2dd88cc02003df6d6f0c93fd58
unknown <saidmf@rpi.edu>
2021-11-09 15:30:18 -0500

    Added single event fixture

--- scripts/tests/test_final_reject.py
+++ scripts/tests/test_final_reject.py
@@ -1,103 +1,73 @@
-from scripts.preprocess import preprocess as pre
-from scripts.data import load, write
-
 import pytest
-
-from pathlib import Path
-
-import mne_bids
-from mne import Epochs
+from scripts.preprocess import preprocess as pre
+import mne
 
 
 @pytest.fixture
-def root():
-    root = Path("CMI/rawdata")
-    return root
+def single_event_data(bids_test_data):
+    # Get events and sampling frequency
+    events, _ = mne.events_from_annotations(bids_test_data)
+    s_freq = bids_test_data.info["sfreq"]
 
+    # Convert events to annotations
+    annotate = mne.annotations_from_events(events, sfreq=s_freq)
 
-@pytest.fixture
-def default_params(root, tmp_path):
-    default_param = write.write_template_params(root, tmp_path)
-    return default_param
-
-
-@pytest.fixture
-def sel_subjects():
-    # should be a random subject?
-    return ["NDARAB793GL3"]
-
-
-@pytest.fixture
-def sel_tasks():
-    # should be a random task?
-    return ["ContrastChangeBlock1"]
-
-
-@pytest.fixture
-def select_data_params(default_params, sel_subjects, sel_tasks):
-    default_params["load_data"]["subjects"] = sel_subjects
-    default_params["load_data"]["tasks"] = sel_tasks
-    return default_params
-
-
-@pytest.fixture
-def error_tasks():
-    return ["Video1"]
-
-
-@pytest.fixture
-def error_data_params(default_params, sel_subjects, error_tasks):
-    default_params["load_data"]["subjects"] = sel_subjects
-    default_params["load_data"]["tasks"] = error_tasks
-    return default_params
+    # Limit annotations to single event and set
+    annotate.delete(list(range(1, annotate.__len__())))
+    single_event_data = bids_test_data.copy().set_annotations(annotate)
+    return single_event_data
 
 
 @pytest.fixture
 def error_obj():
+    # Maybe this should eventually become a busted EEG file
     return None
 
 
-def test_return_values(select_data_params):
-    # Load data using the selected subjects & tasks
-    data = load.load_files(select_data_params["load_data"])
+def test_return_values(default_params, bids_test_data):
+    # Get the default parameters for segmenting and montage
+    feature_params = default_params["preprocess"]
+    seg_params = feature_params["segment_data"]
+    montage_param = feature_params["set_montage"]
 
-    feature_params = select_data_params["preprocess"]
+    # Run on both preloaded and non-preloaded data
+    eeg_obj = bids_test_data
 
-    for file in data:
-        eeg_obj = mne_bids.read_raw_bids(file)
+    # Set the montage file
+    eeg_obj, _ = pre.set_montage(eeg_obj, **montage_param)
 
-        # generate epoched object to be rejected
-        epo, _ = pre.segment_data(eeg_obj, **feature_params["segment_data"])
-        # reject epochs
-        epo.load_data()
-        rej_epo, output_dict = pre.final_reject_epoch(epo)
+    # Generate epoched object to be rejected
+    epo, _ = pre.segment_data(eeg_obj, **seg_params)
+    # Reject epochs using ar
+    rej_epo, output_dict = pre.final_reject_epoch(epo)
 
-        # assert that data is valid
-        assert None not in output_dict.values()
-        assert isinstance(rej_epo, Epochs)
+    # assert that data is valid
+    assert None not in output_dict.values()
+    assert isinstance(rej_epo, mne.Epochs)
 
 
-def test_except_bad_object(error_obj):
+def test_bad_object(error_obj):
     # attempt to filter data w/invalid data
     _, output = pre.final_reject_epoch(error_obj)
     assert "ERROR" in output.keys()
 
 
-def test_except_value(error_data_params):
+def test_single_event(default_params, single_event_data):
+    # Get the default parameters for segmenting
+    feature_params = default_params["preprocess"]
+    seg_params = feature_params["segment_data"]
+    montage_param = feature_params["set_montage"]
 
-    # Load data using the selected subjects & tasks
-    data = load.load_files(error_data_params["load_data"])
+    # Run on both preloaded and non-preloaded data
+    eeg_obj = single_event_data
 
-    # get the pipeline steps
-    feature_params = error_data_params["preprocess"]
+    # Set the montage file
+    eeg_obj, _ = pre.set_montage(eeg_obj, **montage_param)
 
-    for file in data:
-        eeg_obj = mne_bids.read_raw_bids(file)
+    # generate epoched object to be rejected
+    epo, _ = pre.segment_data(eeg_obj, **seg_params)
 
-        # generate epoched object to be rejected
-        epo, _ = pre.segment_data(eeg_obj, **feature_params["segment_data"])
-
-        # attempt to reject epochs with data containing only one entire epoch
-        # across each channel
-        _, output = pre.final_reject_epoch(epo)
-        assert "ERROR" in output.keys()
+    # attempt to reject epochs with data containing only one entire epoch
+    # across each channel
+    _, output = pre.final_reject_epoch(epo)
+    assert "ERROR" in output.keys()
