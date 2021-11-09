b8547d5d1052243e7ee9087d57e3fc132a556994
unknown <saidmf@rpi.edu>
2021-11-09 15:30:39 -0500

    Added default_params to conftest

--- scripts/tests/conftest.py
+++ scripts/tests/conftest.py
@@ -2,6 +2,8 @@ import mne_bids
 import pytest
 import os
 
+from scripts.data import write
+
 
 @pytest.fixture(params=['preloaded_data', 'non_preloaded_data'])
 def bids_test_data(request):
@@ -17,3 +19,11 @@ def bids_test_data(request):
     if request.param == 'preloaded_data':
         raw_test_data.load_data()
     return raw_test_data
+
+
+@pytest.fixture
+def default_params(tmp_path):
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    bids_root = f'{root_dir}/test_bids_data/'
+    default_params = write.write_template_params(bids_root, tmp_path)
+    return default_params
