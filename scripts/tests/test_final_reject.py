62088b89b47481eb3850ee968d3bd22a9029c887
unknown <saidmf@rpi.edu>
2021-11-09 18:15:30 -0500

    Set epoch as fixture for reuse

--- scripts/tests/test_final_reject.py
+++ scripts/tests/test_final_reject.py
@@ -4,7 +4,12 @@ import mne
 
 
 @pytest.fixture
-def single_event_data(bids_test_data):
+def single_event_epoch_data(default_params, bids_test_data):
+    # Get the default parameters for segmenting
+    feature_params = default_params["preprocess"]
+    seg_params = feature_params["segment_data"]
+    montage_param = feature_params["set_montage"]
+
     # Get events and sampling frequency
     events, _ = mne.events_from_annotations(bids_test_data)
     s_freq = bids_test_data.info["sfreq"]
@@ -15,17 +20,22 @@ def single_event_data(bids_test_data):
     # Limit annotations to single event and set
     annotate.delete(list(range(1, annotate.__len__())))
     single_event_data = bids_test_data.copy().set_annotations(annotate)
-    return single_event_data
+
+    # Run on both preloaded and non-preloaded data
+    eeg_obj = single_event_data
+
+    # Set the montage file
+    eeg_obj, _ = pre.set_montage(eeg_obj, **montage_param)
+
+    # generate epoched object to be rejected
+    epo, _ = pre.segment_data(eeg_obj, **seg_params)
+
+    return epo
 
 
 @pytest.fixture
-def error_obj():
-    # Maybe this should eventually become a busted EEG file
-    return None
-
-
-def test_return_values(default_params, bids_test_data):
-    # Get the default parameters for segmenting and montage
+def epoch_data(default_params, bids_test_data):
+    # Get montage and segment params
     feature_params = default_params["preprocess"]
     seg_params = feature_params["segment_data"]
     montage_param = feature_params["set_montage"]
@@ -38,8 +48,19 @@ def test_return_values(default_params, bids_test_data):
 
     # Generate epoched object to be rejected
     epo, _ = pre.segment_data(eeg_obj, **seg_params)
+
+    return epo
+
+
+@pytest.fixture
+def error_obj():
+    # Maybe this should eventually become a busted EEG file
+    return None
+
+
+def test_return_values(epoch_data):
     # Reject epochs using ar
-    rej_epo, output_dict = pre.final_reject_epoch(epo)
+    rej_epo, output_dict = pre.final_reject_epoch(epoch_data)
 
     # assert that data is valid
     assert "ERROR" not in output_dict.keys()
@@ -52,22 +73,8 @@ def test_bad_object(error_obj):
     assert "ERROR" in output.keys()
 
 
-def test_single_event(default_params, single_event_data):
-    # Get the default parameters for segmenting
-    feature_params = default_params["preprocess"]
-    seg_params = feature_params["segment_data"]
-    montage_param = feature_params["set_montage"]
-
-    # Run on both preloaded and non-preloaded data
-    eeg_obj = single_event_data
-
-    # Set the montage file
-    eeg_obj, _ = pre.set_montage(eeg_obj, **montage_param)
-
-    # generate epoched object to be rejected
-    epo, _ = pre.segment_data(eeg_obj, **seg_params)
-
+def test_single_event(single_event_epoch_data):
     # attempt to reject epochs with data containing only one entire epoch
     # across each channel
-    _, output = pre.final_reject_epoch(epo)
+    _, output = pre.final_reject_epoch(single_event_epoch_data)
     assert "ERROR" in output.keys()
