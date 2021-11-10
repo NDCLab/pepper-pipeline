384a770c35d39410703c9c07020e55c31787fb02
unknown <saidmf@rpi.edu>
2021-11-10 13:57:16 -0500

    Reconfig test to utilize conftest

--- scripts/tests/test_write_eeg.py
+++ scripts/tests/test_write_eeg.py
@@ -2,51 +2,21 @@ from scripts.data import write, load
 
 import pytest
 
-from pathlib import Path
 import os
 
 import mne_bids
 
 
 @pytest.fixture
-def root():
-    root = Path("CMI/rawdata")
-    return root
-
-
-@pytest.fixture
-def default_params(root, tmp_path):
-    default_param = write.write_template_params(root, tmp_path)
-    return default_param
-
-
-@pytest.fixture
-def sel_subjects():
-    return ["NDARAB793GL3"]
-
-
-@pytest.fixture
-def sel_tasks():
-    return ["ContrastChangeBlock1"]
-
-
-@pytest.fixture
-def select_data_params(default_params, sel_subjects, sel_tasks):
-    default_params["load_data"]["subjects"] = sel_subjects
-    default_params["load_data"]["tasks"] = sel_tasks
+def overwrite_params(default_params):
+    default_params["load_data"]["overwrite"] = False
     return default_params
 
 
 @pytest.fixture
-def overwrite_params(select_data_params):
-    select_data_params["load_data"]["overwrite"] = False
-    return select_data_params
-
-
-@pytest.fixture
-def non_path_params(select_data_params, tmp_path):
-    select_data_params["output_data"]["root"] = tmp_path / "CMI"
-    return select_data_params
+def non_path_params(default_params, tmp_path):
+    default_params["output_data"]["root"] = tmp_path / "CMI"
+    return default_params
 
 
 @pytest.fixture
@@ -54,9 +24,9 @@ def tmp_func():
     return "TEMP"
 
 
-def test_write(select_data_params, tmp_func):
-    data_params = select_data_params["load_data"]
-    output_params = select_data_params["output_data"]
+def test_write(default_params, tmp_func):
+    data_params = default_params["load_data"]
+    output_params = default_params["output_data"]
 
     ch_type = data_params["channel-type"]
     rewrite = data_params["overwrite"]
@@ -73,9 +43,9 @@ def test_write(select_data_params, tmp_func):
                 assert len(filenames) == 1
 
 
-def test_overwrite(select_data_params, tmp_func):
-    data_params = select_data_params["load_data"]
-    output_params = select_data_params["output_data"]
+def test_overwrite(default_params, tmp_func):
+    data_params = default_params["load_data"]
+    output_params = default_params["output_data"]
 
     ch_type = data_params["channel-type"]
     rewrite = data_params["overwrite"]
