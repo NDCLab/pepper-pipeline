e7da1ec23e864c99434ce2ddc9b79da218c6eb3f
unknown <saidmf@rpi.edu>
2021-11-09 19:59:18 -0500

    Removed epoch fixture in favor for conftest fixture

--- scripts/tests/test_interp_data.py
+++ scripts/tests/test_interp_data.py
@@ -1,41 +1,6 @@
-from scripts.preprocess import preprocess as pre
-from scripts.data import load, write
-
 import pytest
-
-from pathlib import Path
-
-import mne_bids
+from scripts.preprocess import preprocess as pre
 from mne import Epochs
-
-
-@pytest.fixture
-def root():
-    root = Path("CMI/rawdata")
-    return root
-
-
-@pytest.fixture
-def default_params(root, tmp_path):
-    default_param = write.write_template_params(root, tmp_path)
-    return default_param
-
-
-@pytest.fixture
-def sel_subjects():
-    return ["NDARAB793GL3"]
-
-
-@pytest.fixture
-def sel_tasks():
-    return ["ContrastChangeBlock1"]
-
-
-@pytest.fixture
-def select_data_params(default_params, sel_subjects, sel_tasks):
-    default_params["load_data"]["subjects"] = sel_subjects
-    default_params["load_data"]["tasks"] = sel_tasks
-    return default_params
 
 
 @pytest.fixture
@@ -43,34 +8,22 @@ def error_obj():
     return None
 
 
-def test_return_values(select_data_params):
-    # Load data using the selected subjects & tasks
-    data = load.load_files(select_data_params["load_data"])
+def test_return_values(default_params, bids_test_epoch_data):
+    epoch_data = bids_test_epoch_data
+    # interpolate data
+    feature_params = default_params["preprocess"]
+    interp_params = feature_params["interpolate_data"]
+    interp_eeg, output_dict = pre.interpolate_data(epoch_data, **interp_params)
 
-    # get the pipeline steps
-    feature_params = select_data_params["preprocess"]
-    interp_param = feature_params["interpolate_data"]
-
-    for file in data:
-        eeg_obj = mne_bids.read_raw_bids(file)
-
-        # generate epoched object to be interpolated
-        epo, _ = pre.segment_data(eeg_obj, **feature_params["segment_data"])
-
-        # interpolate data
-        epo.load_data()
-        interp_eeg, output_dict = pre.interpolate_data(epo, **interp_param)
-
-        # assert that all data is valid
-        assert None not in output_dict.values()
-        assert isinstance(interp_eeg, Epochs)
+    # assert that all data is valid
+    assert "ERROR" not in output_dict.keys()
+    assert isinstance(interp_eeg, Epochs)
 
 
-def test_except_value(error_obj, select_data_params):
-    feature_params = select_data_params["preprocess"]
+def test_bad_object(default_params, error_obj):
+    feature_params = default_params["preprocess"]
     interp_param = feature_params["interpolate_data"]
 
     # attempt to interpolate an invalid object type
-    # across each channel
     _, output = pre.interpolate_data(error_obj, **interp_param)
     assert "ERROR" in output.keys()
