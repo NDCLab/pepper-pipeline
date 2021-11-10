2b210894278a85b3385c8a042b6d788ec342444e
unknown <saidmf@rpi.edu>
2021-11-09 19:46:05 -0500

    Added epoch data to conftest

--- scripts/tests/conftest.py
+++ scripts/tests/conftest.py
@@ -3,10 +3,11 @@ import pytest
 import os
 
 from scripts.data import write
+from scripts.preprocess import preprocess as pre
 
 
-@pytest.fixture(params=['preloaded_data', 'non_preloaded_data'])
-def bids_test_data(request):
+@pytest.fixture
+def raw_data():
     root_dir = os.path.dirname(os.path.abspath(__file__))
     bids_root = f'{root_dir}/test_bids_data/'
     bids_path = mne_bids.BIDSPath(subject='testdata',
@@ -16,9 +17,36 @@ def bids_test_data(request):
                                   datatype='eeg',
                                   root=bids_root)
     raw_test_data = mne_bids.read_raw_bids(bids_path)
-    if request.param == 'preloaded_data':
-        raw_test_data.load_data()
     return raw_test_data
+
+
+@pytest.fixture(params=['preloaded_data', 'non_preloaded_data'])
+def bids_test_data(request, raw_data):
+    if request.param == 'preloaded_data':
+        raw_data.load_data()
+    return raw_data
+
+
+@pytest.fixture(params=['preloaded_data', 'non_preloaded_data'])
+def bids_test_epoch_data(request, raw_data, default_params):
+    # Get montage and segment params
+    feature_params = default_params["preprocess"]
+    seg_params = feature_params["segment_data"]
+    montage_param = feature_params["set_montage"]
+
+    # Run on both preloaded and non-preloaded data
+    eeg_obj = bids_test_data
+
+    # Set the montage file
+    eeg_obj, _ = pre.set_montage(raw_data, **montage_param)
+
+    # Generate epoched object
+    epo, _ = pre.segment_data(eeg_obj, **seg_params)
+
+    if request.param == 'preloaded_data':
+        epo.load_data()
+
+    return epo
 
 
 @pytest.fixture
