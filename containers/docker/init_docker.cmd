a5e60647d0c00b05c41aecfc686aa2b3ac69d861
unknown <saidmf@rpi.edu>
2021-11-11 13:43:24 -0500

    Added in git metadata to docker container

--- containers/docker/init_docker.cmd
+++ containers/docker/init_docker.cmd
@@ -10,8 +10,11 @@ docker cp ../../run.py pepper:/projects/pepper-pipeline
 docker cp ../../CONTRIBUTING.md pepper:/projects/pepper-pipeline
 docker cp ../../README.md pepper:/projects/pepper-pipeline
 docker cp ../../user_params.json pepper:/projects/pepper-pipeline
+
+:: copy in git metadata
 docker cp ../../.github pepper:/projects/pepper-pipeline
 docker cp ../../.git pepper:/projects/pepper-pipeline
+docker cp ../../.gitignore pepper:/projects/pepper-pipeline
 
 :: run container instance
 docker exec -u root -it pepper bash
