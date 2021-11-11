e74cc0ef929651c811e7c4838dce5bcc0a27aa50
unknown <saidmf@rpi.edu>
2021-11-11 10:21:22 -0500

    Added project specific folder

--- containers/docker/init_docker.cmd
+++ containers/docker/init_docker.cmd
@@ -2,16 +2,16 @@
 docker volume create data
 
 :: bind the volume by running the container with the specified tags
-docker run -it -d --name pepper -w /projects --mount source=data,destination=/projects %1
+docker run -it -d --name pepper -w /projects/pepper-pipeline --mount source=data,destination=/projects %1
 
 :: copy all data from local to container
-docker cp ../../scripts pepper:/projects
-docker cp ../../run.py pepper:/projects
-docker cp ../../CONTRIBUTING.md pepper:/projects
-docker cp ../../README.md pepper:/projects
-docker cp ../../user_params.json pepper:/projects
-docker cp ../../.github pepper:/projects
-docker cp ../../.git pepper:/projects
+docker cp ../../scripts pepper:/projects/pepper-pipeline
+docker cp ../../run.py pepper:/projects/pepper-pipeline
+docker cp ../../CONTRIBUTING.md pepper:/projects/pepper-pipeline
+docker cp ../../README.md pepper:/projects/pepper-pipeline
+docker cp ../../user_params.json pepper:/projects/pepper-pipeline
+docker cp ../../.github pepper:/projects/pepper-pipeline
+docker cp ../../.git pepper:/projects/pepper-pipeline
 
 :: run container instance
 docker exec -u root -it pepper bash
