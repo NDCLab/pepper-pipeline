368e93bac584047e4870327cf7c19c00d9c3567b
unknown <saidmf@rpi.edu>
2021-11-11 19:05:11 -0500

    Combined split_half with trial_erp

--- scripts/postprocess/run_metrics.py
+++ scripts/postprocess/run_metrics.py
@@ -1,7 +1,9 @@
 import os
 import postprocess
 
-data_path = "/Users/yanbinniu/Documents/Projects/tf-pca/OSF/tfpca-tutorial/ptb_data/"
+from scripts.reliability.reliability import split_half
+
+data_path = "/home/data/NDClab/datasets/cmi-dataset/derivatives/preprocessed"
 
 fileList = []
 for root, dirs, files in os.walk(data_path):
@@ -12,7 +14,9 @@ for root, dirs, files in os.walk(data_path):
 # compute splithalf reliability
 # get trial level erp for all participants
 datalist1, datalist2 = postprocess.get_trial_erp(fileList, 0, 0.1, '12', '5')
+
 # call Dan's splithalf
+print(split_half(datalist1, datalist2))
 
 # compute SME
 sme = postprocess.sme(fileList, 0, 0.1, ['12', '5'], ['E1', 'E2'])
