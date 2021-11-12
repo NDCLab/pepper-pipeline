529b967020f2aa0c76fcb710323b041b9502079c
unknown <saidmf@rpi.edu>
2021-11-11 19:06:53 -0500

    Format reliability.py in accordance with pep8

--- scripts/reliability/reliability.py
+++ scripts/reliability/reliability.py
@@ -3,7 +3,8 @@ from collections import namedtuple
 
 
 def spearman_brown(correlation_coefficient):
-    """Apply Spearman-Brown Correction, to convert a Pearson correlation to measure of reliability.
+    """Apply Spearman-Brown Correction, to convert a Pearson correlation to
+    measure of reliability.
     Parameters
     ----------
     correlation_coefficient: float
@@ -18,14 +19,17 @@ def spearman_brown(correlation_coefficient):
 
 
 def split_half(data_group_one, data_group_two=None, number_permutations=5000):
-    """Compute split-half reliability for either a single measure, or a difference between two measures.
+    """Compute split-half reliability for either a single measure, or a
+    difference between two measures.
     Parameters
     ----------
     data_group_one: list
-                    A list of 1D numpy arrays. Each array represents data from one group.
+                    A list of 1D numpy arrays. Each array represents data from
+                    one group.
 
     data_group_two: list, optional
-                    A list of 1D numpy arrays. Each array represents data from one group.
+                    A list of 1D numpy arrays. Each array represents data from
+                    one group.
 
     number_permutations: int, optional
                          The number of split half permutations to compute
@@ -33,7 +37,8 @@ def split_half(data_group_one, data_group_two=None, number_permutations=5000):
     Returns
     ----------
     namedtuple
-        For both correlation and reliability, the mean, lower, and upper 95% CI of the distribution across permutations
+        For both correlation and reliability, the mean, lower, and upper 95%
+        CI of the distribution across permutations
     """
 
     if data_group_two is None:
@@ -42,10 +47,13 @@ def split_half(data_group_one, data_group_two=None, number_permutations=5000):
         compute_difference = True
 
     if compute_difference and (len(data_group_one) != len(data_group_two)):
-        raise ValueError('When computing a difference, data_group_one and data_group_two must be the same length')
+        raise ValueError(
+            'When computing a difference, data_group_one and data_group_two \
+             must be the same length')
 
     distribution = namedtuple('distribution', ['mean', 'lower', 'upper'])
-    split_half_result = namedtuple('split_half_result', ['correlation', 'reliability'])
+    split_half_result = namedtuple(
+        'split_half_result', ['correlation', 'reliability'])
 
     rng = np.random.default_rng()
 
@@ -53,16 +61,27 @@ def split_half(data_group_one, data_group_two=None, number_permutations=5000):
     sb_reliability = np.zeros(number_permutations)
 
     for n in range(number_permutations):
-        data_random_split = [np.array_split(rng.permutation(d), 2) for d in data_group_one]  # permute each array, split
-        mean_by_split = np.array([[np.mean(split) for split in group] for group in data_random_split])  # mean of each split
-        if compute_difference:  # do the same for the second group of data if computing difference
-            data_random_split_two = [np.array_split(rng.permutation(d), 2) for d in data_group_two]
-            mean_by_split_two = np.array([[np.mean(split) for split in group] for group in data_random_split_two])
+        data_random_split = [np.array_split(rng.permutation(
+            d), 2) for d in data_group_one]  # permute each array, split
+        # mean of each split
+        mean_by_split = np.array([[np.mean(split) for split in group]
+                                  for group in data_random_split])
+        # do the same for the second group of data if computing difference
+        if compute_difference:
+            data_random_split_two = [np.array_split(
+                rng.permutation(d), 2) for d in data_group_two]
+            mean_by_split_two = np.array(
+                [[np.mean(split) for split in group] for group in
+                 data_random_split_two])
             mean_by_split = mean_by_split - mean_by_split_two
-        corr_value = np.corrcoef(mean_by_split, rowvar=False)[0, 1]  # [0,1] to retain 1 value from sym matrix
+        # [0,1] to retain 1 value from sym matrix
+        corr_value = np.corrcoef(mean_by_split, rowvar=False)[0, 1]
         corr_coefs[n] = corr_value
-        sb_reliability[n] = spearman_brown(corr_value)  # Spearman-Brown correction
+        sb_reliability[n] = spearman_brown(
+            corr_value)  # Spearman-Brown correction
 
-    correlation_dist = distribution(np.mean(corr_coefs), np.quantile(corr_coefs, .025), np.quantile(corr_coefs, .975))
-    reliability_dist = distribution(np.mean(sb_reliability), np.quantile(sb_reliability, .025), np.quantile(sb_reliability, .975))
+    correlation_dist = distribution(np.mean(corr_coefs), np.quantile(
+        corr_coefs, .025), np.quantile(corr_coefs, .975))
+    reliability_dist = distribution(np.mean(sb_reliability), np.quantile(
+        sb_reliability, .025), np.quantile(sb_reliability, .975))
     return split_half_result(correlation_dist, reliability_dist)
