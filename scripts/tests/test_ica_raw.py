1eb65aa7b7dc908de8c6b632d1d33011a31bf73c
unknown <f.saidmuratov@yahoo.com>
2021-11-15 15:50:47 -0500

    Removed fixtures and dictionary check in ica_raw

--- scripts/tests/test_ica_raw.py
+++ scripts/tests/test_ica_raw.py
@@ -1,14 +1,5 @@
-
-import pytest
 from scripts.preprocess import preprocess as pre
 from mne.io import BaseRaw
-
-from scripts.constants import ERROR_KEY
-
-
-@pytest.fixture
-def error_obj():
-    return None
 
 
 def test_return_values(default_params, bids_test_data):
@@ -28,36 +19,7 @@ def test_return_values(default_params, bids_test_data):
     filt_obj, _ = pre.filter_data(eeg_obj, **filt_param)
 
     # apply ica to filtered eeg object
-    ica_obj, output_dict = pre.ica_raw(filt_obj, **ica_param)
+    ica_obj, _ = pre.ica_raw(filt_obj, **ica_param)
 
     # assert that None does not exist in bad chans
-    assert "ERROR" not in output_dict.keys()
     assert isinstance(ica_obj, BaseRaw)
-
-
-def test_bad_object(default_params, error_obj):
-    feature_params = default_params["preprocess"]
-    ica_param = feature_params["ica_raw"]
-
-    # attempt to process ica w/invalid data
-    _, output = pre.ica_raw(error_obj, **ica_param)
-    assert ERROR_KEY in output.keys()
-
-
-def test_missing_montage(default_params, bids_test_data):
-    # get default pipeline params
-    feature_params = default_params["preprocess"]
-
-    ica_param = feature_params["ica_raw"]
-    filt_param = feature_params["filter_data"]
-
-    eeg_obj = bids_test_data
-
-    # filter
-    filt_obj, _ = pre.filter_data(eeg_obj, **filt_param)
-
-    # apply ica to raw filt object with no montage file
-    _, output = pre.ica_raw(filt_obj, **ica_param)
-
-    # attempt to process ica w/invalid data
-    assert ERROR_KEY in output.keys()
