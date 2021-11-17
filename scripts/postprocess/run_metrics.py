3c9fc777d9eeb18ab1bd5c352de0e004d698ee99
Farukh Said <saidmf@rpi.edu>
2021-11-17 12:58:52 -0500

    Added code to write csv file and process specific tasks

--- scripts/postprocess/run_metrics.py
+++ scripts/postprocess/run_metrics.py
@@ -1,22 +1,35 @@
 import os
 import postprocess
+from reliability import split_half
+import sys
+import csv
 
-from scripts.reliability.reliability import split_half
-
-data_path = "/home/data/NDClab/datasets/cmi-dataset/derivatives/preprocessed"
-
+data_path = "/home/data/NDClab/datasets/cmi-dataset/derivatives/PEPPER_pipeline/PEPPER_pipeline_preprocessed"
+original_stdout = sys.stdout
 fileList = []
 for root, dirs, files in os.walk(data_path):
     for file in files:
-        if file.endswith('.fif'):
+        if file.endswith('.fif') and "surroundSuppBlock" in file:
             fileList.append(os.path.join(root, file))
 
 # compute splithalf reliability
 # get trial level erp for all participants
-datalist1, datalist2 = postprocess.get_trial_erp(fileList, 0, 0.1, '12', '5')
+datalist1, _ = postprocess.get_trial_erp(fileList, 0.15, 0.2, '4')
 
 # call Dan's splithalf
-print(split_half(datalist1, datalist2))
+split = split_half(datalist1, None)
+with open('split_half_50.txt', 'w') as f:
+    sys.stdout = f
+    f.write("Split half measure\n")
+    print(split)
+    sys.stdout = original_stdout
+    
 
 # compute SME
-sme = postprocess.sme(fileList, 0, 0.1, ['12', '5'], ['E1', 'E2'])
+sme = postprocess.sme(fileList, 0.15, 0.2, ['4'], ['E75'])
+with open('sme_50.csv', 'w') as f:
+    # unpack sme
+    unpacked = [ str(val[0]) for val in sme ]
+    print(unpacked)
+    write = csv.writer(f)
+    write.writerow(unpacked)
