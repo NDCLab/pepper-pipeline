92d0746c883cc485d8933656adfa51261f9fc755
unknown <f.saidmuratov@yahoo.com>
2021-11-18 10:10:30 -0500

    Implemented starmap to create parallel runs using all args

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+import mne_bids
 from scripts.data import load, write
 from scripts.preprocess import preprocess as pre
 
 from collections import ChainMap
-import mne_bids
+from itertools import repeat
 
 import logging
 from multiprocessing import Pool
@@ -16,6 +17,42 @@ from scripts.constants import \
 def clean_outputs(output_dict):
     output_dict = [result for result in output_dict if result is not None]
     return output_dict
+
+
+def preprocess_data(file, preprocess, ch_type, exit_on_error, rewrite, path):
+    """Function to preprocess data
+    """
+    # load raw data from file
+    eeg_obj = mne_bids.read_raw_bids(file)
+    # initialize output list
+    outputs = [None] * len(preprocess)
+
+    # For each pipeline step, execute with parameters
+    for idx, (func, params) in enumerate(preprocess.items()):
+        try:
+            eeg_obj, outputs[idx] = getattr(pre, func)(eeg_obj, **params)
+        except Exception as e:
+            # On error, replace output with exception
+            outputs[idx] = {func: ERROR_KEY + str(e)}
+            # Remove all un-filled outputs
+            outputs = clean_outputs(outputs)
+
+            # Exit pipeline or skip subject
+            if exit_on_error:
+                sys.exit(EXIT_MESSAGE)
+            logging.info(CAUGHT_EXCEPTION_SKIP)
+            return
+
+        # check if this is the final preprocessed eeg object
+        final = (idx == len(preprocess.items()) - 1)
+        # write object out to file
+        write.write_eeg_data(eeg_obj, func, file, ch_type, final, path,
+                             rewrite)
+
+    # collect annotations of each step
+    outputs.reverse()
+    output = dict(ChainMap(*outputs))
+    write.write_output_param(output, file, ch_type, path, rewrite)
 
 
 def run_pipeline(preprocess, load_data, write_data):
@@ -32,57 +69,23 @@ def run_pipeline(preprocess, load_data, write_data):
                 Dictionary containing parameters to write data
 
     """
-    def preprocess_data(file):
-        """Nested function to preprocess data
-        """
-        # load raw data from file
-        eeg_obj = mne_bids.read_raw_bids(file)
-        # initialize output list
-        outputs = [None] * len(preprocess)
-
-        # For each pipeline step, execute with parameters
-        for idx, (func, params) in enumerate(preprocess.items()):
-            try:
-                eeg_obj, outputs[idx] = getattr(pre, func)(eeg_obj, **params)
-            except Exception as e:
-                # On error, replace output with exception
-                outputs[idx] = {func: ERROR_KEY + str(e)}
-                # Remove all un-filled outputs
-                outputs = clean_outputs(outputs)
-
-                # Exit pipeline or skip subject
-                if exit_on_error:
-                    sys.exit(EXIT_MESSAGE)
-                logging.info(CAUGHT_EXCEPTION_SKIP)
-                return
-
-            # check if this is the final preprocessed eeg object
-            final = (idx == len(preprocess.items()) - 1)
-            # write object out to file
-            write.write_eeg_data(eeg_obj, func, file, ch_type, final, path,
-                                 rewrite)
-
-        # collect annotations of each step
-        outputs.reverse()
-        output = dict(ChainMap(*outputs))
-        write.write_output_param(output, file, ch_type, path, rewrite)
-
     # get pipeline parameters
     ch_type = load_data["channel-type"]
-    exit_on_error = load_data["exit_on_error"]
+    exit_error = load_data["exit_on_error"]
     rewrite = load_data["overwrite"]
-    parallel = load_data["parallel"]
     path = write_data["root"]
 
     # get set of subjects & tasks to run while omitting existing exceptions
     data = load.load_files(load_data)
 
-    # get number of runs based on available cpus and param
-    runs = 1 if parallel else None
+    # get number of workers based on available cpus and param
+    runs = None if load_data["parallel"] else 1
 
+    # parallelize data by executing pipeline steps on each loaded file
     with Pool(runs) as worker:
-        # for each file in filtered data
-        worker.map(preprocess_data, data)
+        worker.starmap(preprocess_data,
+                       zip(data, repeat(preprocess), repeat(ch_type),
+                           repeat(exit_error), repeat(rewrite), repeat(path)))
 
 
 if __name__ == "__main__":
